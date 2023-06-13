bdeff5a9dd914260ffd24ca151ed4362a3e27619
Vikash kumar chaudhary <80942264+vikash217@users.noreply.github.com>
2023-06-13 12:48:33 +0530

    Update Venturesathi-Analytics.py

--- Venturesathi-Analytics.py
+++ Venturesathi-Analytics.py
@@ -6,7 +6,7 @@ import base64
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
 # Set the base URL of the FastAPI server
-BASE_URL = "http://localhost:8000/LSTM_Predict"
+BASE_URL = "http://localhost:8000"
 
 # Load the logo image
 logo_image = "Logo.png"
