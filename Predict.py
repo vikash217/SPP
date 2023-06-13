3f88b69bf2c0d69c7bb9dfa5a661f263df870402
Vikash kumar chaudhary <80942264+vikash217@users.noreply.github.com>
2023-06-13 12:37:16 +0530

    Update Predict.py

--- Predict.py
+++ Predict.py
@@ -20,7 +20,7 @@ STOCK_FILE_PATHS = {
    "Tcs": "TCS.csv",
 }
 
-@app.post("/https://webapp-dyz6.onrender.com/")
+@app.post("/LSTM_Predict")
 async def predict(stock_request: StockRequest):
     stock_name = stock_request.stock_name
     start_date = pd.to_datetime(stock_request.start_date)
