0dcae0c9a3b61923b25a1ed4769870b21da8b7ba
Vikash kumar chaudhary <80942264+vikash217@users.noreply.github.com>
2023-10-31 20:15:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,23 +5,18 @@ import matplotlib.pyplot as plt
 import base64 
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
-# Set the base URL of the FastAPI server
 BASE_URL = "http://localhost:8000"
 
-# Load the logo image
 logo_image = "Logo.png"
 
-# Create the layout
 col1, col2 = st.columns([1, 5])
 with col1:
     st.image(logo_image, width=100)
 with col2:
     st.title("Stock Price Prediction")
 
-# Stock selection
 stock_name = st.selectbox("Select a stock", ("Reliance", "Zomato", "HDFC", "TCS"))
 
-# Date range selection
 start_date = st.date_input("Select start date")
 end_date = st.date_input("Select end date")
 
@@ -30,11 +25,9 @@ if start_date < end_date:
         # Prepare the request payload
         payload = {"stock_name": stock_name, "start_date": str(start_date), "end_date": str(end_date)}
 
-        # Send the request to the server
         response = requests.post(f"{BASE_URL}/LSTM_Predict", json=payload)
 
         if response.status_code == 200:
-            # Display predicted prices
             data = response.json()
             if "future_prediction" in data:
                 predict_prices = data["future_prediction"]
@@ -47,37 +40,9 @@ if start_date < end_date:
                     row = [date] + prices
                     table_data.append(row)
 
-                # Create a DataFrame and remove the column index label
                 df = pd.DataFrame(table_data[1:], columns=table_data[0]).rename_axis(None, axis=1)
 
                 st.table(df)
-                
-                #csv_data = df.to_csv(index=True)
-               # b64 = base64.b64encode(csv_data.encode()).decode()
-               # href = f'<a href="data:file/csv;base64,{b64}" download="predicted_prices.csv">Download CSV</a>'
-                #st.markdown(href, unsafe_allow_html=True)
-
-                # Create line plot chart
-                #plt.figure(figsize=(10, 6))
-                #for i, column_name in enumerate(column_names):
-                 #   plt.plot(future_dates, [price[i] for price in predict_prices], label=column_name)
-                #plt.xlabel("Date")
-                #plt.ylabel("Price")
-                #plt.title(f"Stock Price Trend for {stock_name}")
-               # plt.xticks(rotation=45)
-               # plt.legend()
-               # st.pyplot()
-
-                # Create scatter plot chart
-               # plt.figure(figsize=(10, 6))
-                #for i, column_name in enumerate(column_names):
-                 #   plt.scatter(future_dates, [price[i] for price in predict_prices], label=column_name)
-               # plt.xlabel("Date")
-               # plt.ylabel("Price")
-              #  plt.title(f"Stock Price Scatter Plot for {stock_name}")
-               # plt.xticks(rotation=45)
-               # plt.legend()
-               # st.pyplot()
 
             else:
                 st.error("Error: Failed to retrieve predicted prices.")
