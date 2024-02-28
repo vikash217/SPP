469fa64456ea0f860ae69237ea3d59c456d37689
Vikash kumar chaudhary <80942264+vikash217@users.noreply.github.com>
2024-02-28 22:47:23 +0530

    Update Web.py

--- Web.py
+++ Web.py
@@ -1,19 +1,69 @@
+# import streamlit as st
+# import requests
+# import pandas as pd
+# import base64 
+# st.set_option('deprecation.showPyplotGlobalUse', False)
+
+# BASE_URL = "https://model-591m.onrender.com"
+
+# st.title("Stock Price Prediction")
+
+# stock_name = st.selectbox("Select a stock", ("Reliance", "Zomato", "HDFC", "TCS"))
+
+# start_date = st.date_input("Select start date")
+# end_date = st.date_input("Select end date")
+
+# if start_date <= end_date:
+#     if st.button("Predict"):
+#         payload = {"stock_name": stock_name, "start_date": str(start_date), "end_date": str(end_date)}
+
+#         response = requests.post(f"{BASE_URL}/LSTM_Predict", json=payload)
+
+#         if response.status_code == 200:
+#             data = response.json()
+#             if "future_prediction" in data:
+#                 predict_prices = data["future_prediction"]
+#                 future_dates = data["future_dates"]
+#                 column_names = data["column_names"]
+
+#                 st.write(f"Predicted prices for {stock_name} between {start_date} and {end_date}:")
+#                 table_data = [["Date"] + column_names] 
+#                 for date, prices in zip(future_dates, predict_prices):
+#                     row = [date] + prices
+#                     table_data.append(row)
+
+#                 df = pd.DataFrame(table_data[1:], columns=table_data[0]).rename_axis(None, axis=1)
+
+#                 st.table(df)
+
+#             else:
+#                 st.error("Error: Failed to retrieve predicted prices.")
+#         else:
+#             st.error("Error: Failed to connect to the server.")
+# else:
+#     st.error("Error: The start date must be before the end date.")
+
 import streamlit as st
 import requests
 import pandas as pd
-import base64 
+import matplotlib.pyplot as plt
+import base64
+
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
 BASE_URL = "https://model-591m.onrender.com"
 
 st.title("Stock Price Prediction")
 
-stock_name = st.selectbox("Select a stock", ("Reliance", "Zomato", "HDFC", "TCS"))
+# Provide stock options as a list
+stock_options = ["Zomato"]
+
+stock_name = st.selectbox("Select stock", stock_options)
 
 start_date = st.date_input("Select start date")
 end_date = st.date_input("Select end date")
 
-if start_date <= end_date:
+if start_date < end_date:
     if st.button("Predict"):
         payload = {"stock_name": stock_name, "start_date": str(start_date), "end_date": str(end_date)}
 
@@ -40,5 +90,6 @@ if start_date <= end_date:
                 st.error("Error: Failed to retrieve predicted prices.")
         else:
             st.error("Error: Failed to connect to the server.")
-# else:
-#     st.error("Error: The start date must be before the end date.")
+else:
+    st.error("Error: The start date must be before the end date.")
+
